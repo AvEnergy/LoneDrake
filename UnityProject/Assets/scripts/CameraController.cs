9b7788b9f35875eec9e20ef4135c0dce3bcf7e9d
Emerson McCabe <137830363+AvEnergy@users.noreply.github.com>
2024-05-23 21:11:39 -0400

    RunningAnimations
    Particle system set up when sprinting, FoV changes to make the player feel that they're moving faster.
    
    Small update to flamethrower so that it follows the world to give a better appearance.

--- UnityProject/Assets/scripts/CameraController.cs
+++ UnityProject/Assets/scripts/CameraController.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    [SerializeField] Camera _camera;
     [SerializeField] int sensitivity;
     [SerializeField] int lockVertmin, lockVertmax;
     [SerializeField] bool invertY;
+    [SerializeField] ParticleSystem runningParticles;
 
     float rotX;
     // Start is called before the first frame update
@@ -19,6 +21,16 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetButton("Sprint") && !gameManager.instance.isPaused)
+        {
+            _camera.fieldOfView = Mathf.Lerp(_camera.fieldOfView, 70f, 5f * Time.deltaTime);
+            runningParticles.Play();
+        }
+        else
+        {
+            _camera.fieldOfView = Mathf.Lerp(_camera.fieldOfView, 50f, 5f * Time.deltaTime);
+            runningParticles.Stop();
+        }
 
         //Get input
         float mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * sensitivity;
